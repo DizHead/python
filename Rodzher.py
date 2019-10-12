19c243cd9f1e4793e5d94d37daa855488997e517
lyubomyr83 <lyubomyr83@mail.ru>
2019-10-12 18:00:44 +0300

    Rodjer in progress

--- Rodzher.py
+++ Rodzher.py
@@ -22,7 +22,11 @@ while ready not in {'да', 'нет'}:
     ready = input()
 
 if ready == 'да':
-   answers_quantity = ''
+
+   answers_quantity = ''  # количество примеров
+   maximum_answer = ''  # до скольки будет считать
+   question = ''
+
 
    while not answers_quantity.isdigit():
        print(name + ", сколько примеров ты готов решить?")
@@ -32,33 +36,53 @@ if ready == 'да':
            while int(answers_quantity) < 1:
                print ("Введи число больше 0")
                answers_quantity = input()
+               while not maximum_answer.isdigit():
+                   print("Должна быть цифра")
+                   answers_quantity = input()
        else:
            print("Должна быть цифра")
 
+   while not maximum_answer.isdigit():
+       print("До скольки будем считать")
+       maximum_answer = input()
 
-   print("До скольки будет решать")
-   maximum_answer = int(input())
+       if maximum_answer.isdigit():
+           while int(maximum_answer) < 2:
+               print("Введи число больше 1")
+               maximum_answer = input()
+               while not maximum_answer.isdigit():
+                   print("Должна быть цифра")
+                   maximum_answer = input()
+       else:
+           print("Должна быть цифра")
 
-   while maximum_answer <2:
-        print("Введи  число  больше 1")
-        maximum_answer = int(input())
+       for question in range(1, int(answers_quantity)):
+           print("Пример " + str(question+1))
 
-   # случайным образом сгенерируем
-   numeric1 = randint(1, maximum_answer)  # левый операнд
-   numeric2 = randint(1, maximum_answer)  # правый операнд
-   sign = choice('+-')  # арифметический оператор
-   # вычислим результат в зависимости от операции
-   if sign == '-':
-        # исключим отрицательный ответ
-        while numeric1 < numeric2:
-            numeric1 = randint(1, maximum_answer)
-        correct_answer = numeric1 - numeric2
-   if sign == '+':
-        # исключим превышение максимально допустимого ответа
-        while numeric1 + numeric2 > maximum_answer:
-            numeric1 = randint(1, maximum_answer)
-            numeric2 = randint(1, maximum_answer)
-            correct_answer = numeric1 + numeric2
+           # случайным образом сгенерируем
+           numeric1 = randint(1, int(maximum_answer))  # левый операнд
+           numeric2 = randint(1, int(maximum_answer))  # правый операнд
+           sign = choice('+-')  # арифметический оператор
+
+           # вычислим результат в зависимости от операции
+           if sign == '-':
+               # исключим отрицательный ответ
+               while numeric1 < numeric2:
+                   numeric1 = randint(1, int(maximum_answer))
+                   correct_answer = numeric1 - numeric2
+           if sign == '+':
+               # исключим превышение максимально допустимого ответа
+               while numeric1 + numeric2 > int(maximum_answer):
+                   numeric1 = randint(1, int(maximum_answer))
+                   numeric2 = randint(1, int(maximum_answer))
+                   correct_answer = numeric1 + numeric2
+           print("сколько будет " + str(numeric1) +str(sign) +str(numeric2))
+           student_answer = input()
+           if student_answer == correct_answer:
+               print("Правильно,молодец!")
+           else:
+               print("Неправильно")
+               print("Правильный ответ: " + correct_answer)
 
 if ready == 'нет':
     print('''Передумал? Хорошо, может как-нибудь в следующий раз...
