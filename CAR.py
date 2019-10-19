be0ed59d77a53644ddec3b63deb7aa428dc47a8a
lyubomyr83 <lyubomyr83@mail.ru>
2019-10-19 17:59:48 +0300

    Rodjer in progress

---
+++ CAR.py
@@ -0,0 +1,12 @@
+command = input()
+
+if command == 'on':
+    print("Двигатель включен")
+if command == 'off':
+    print("Двигатель выключен")
+if command == 'exit':
+    print("Выход из машины")
+if command == 'Help':
+    print('''    On-Завести двигатель
+    off-Выключить Двигатель
+    Exit-Выход из машины''')
