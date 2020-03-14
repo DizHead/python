037dcb26993db8d7794cc7f31b80acd9f1f9903e
lyubomyr83 <lyubomyr83@mail.ru>
2020-03-14 18:00:53 +0300

    14.03.20

--- __pycache__/21.py
+++ __pycache__/21.py
@@ -8,11 +8,12 @@ dpoints = 0
 move = 1
 more = 'да'
 while more == 'да':
-    dcard = cards.pop()
-    dpoints += dcard
-
-    ycards = []
     if move == 1:
+        dcard = cards.pop()
+        dpoints += dcard
+
+        ycards = []
+    
         for i in range(2):
             ycard = cards.pop()
             ycards.append(ycard)
@@ -36,29 +37,47 @@ while more == 'да':
     if move == 1:
         print(f'Вам выпали карты номиналом {ycards[0]} и {ycards[1]}')
     else:
-        print(f'Вам выпала карта номиналом {ycards[0]}')     
+        print(f'Вам выпала карта номиналом {ycard}')     
 
-    if ypoints & dpoints < 22:
-        if ypoints == 21 or dpoints == 21:
-            if ypoints == 21:
-                print('Блэкджек! Вы выиграли')
-            else:
-                print('Блэкджек у противника! Вы проиграли')
-        elif ypoints == dpoints:
-            print('Ничья')  
-            break
-        elif ypoints < dpoints:
-            print('Вы проиграли')
-            break
-        elif ypoints > dpoints:
-            more = input('Еще? да или нет\n')
-            if not more:
-                if ypoints > dpoints:
-                    print('Вы выиграли')
-                    break
+    if ypoints < 22:
+        if ypoints == 21:
+            more = 'нет'
+        else:
+            more = input('Ещё? Да или нет\n')
+        move +=1
     else:
-        if ypoints < 21:
-            print('Вы выиграли')            
-   
+        print('Перебор')
+        break
+else:
+    while dpoints < 18:
+        dcard = cards.pop()
+        dpoints += dcard
+    print(f'''
+    -------------
+    Очки:
+    
+    Крупъе: {dpoints}
+
+    Вы: {ypoints}
+    -------------
+
+    Крупъе выпала карта номиналом {ycard}    
+
+    ''')
+
+    if ypoints == dpoints:
+        print('Ничья') 
+    elif dpoints > 21:
+        print('Крупъе проиграл. Перебор') 
+    elif ypoints < dpoints:
+        print('Вы проиграли. У крупъе число выше')  
+    elif ypoints > dpoints:
+        print('Вы выиграли. У Вас число выше')      
+    elif dpoints == 21:
+        print('Вы проиграли. У Крупъе блэкджек')
+    elif ypoints == 21:
+        print('Вы выиграли. У Вас блэкджек')
+    elif ypoints > 21:
+        print('Вы проиграли. Перебор')
+          
         
-    move += 1
